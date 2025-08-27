bbab62c8d90777842520e768eaba1d529395bf96
JulianAvella <julianavella01@gmail.com>
2025-08-27 14:47:52 -0500

    feat: Se ajustan los environments para despliegue udistrital/sga_documentacion#626

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,13 +1,13 @@
 export const environment = {
     production: true,
-    apiUrl: "https://verificacion_externa.portaloas.udistrital.edu.co/",
+    apiUrl: "https://verificacionexterna.portaloas.udistrital.edu.co",
     AUTENTICACION_MID: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/',
     FIRMA_ELECTRONICA_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/firma_electronica_mid/v1/',
-    VERIFICACION_FIRMA_SERVICE: 'http://localhost:8567/v1/',
+    VERIFICACION_FIRMA_SERVICE: 'http://pruebasapi3.intranetoas.udistrital.edu.co:8584/v1/',
     SECRET_KEY_: 'JuMusTq5jWh8yIdTeqQxN7uYt6Ia',
     TOKEN: {
         CLIENTE_ID: 'q5NoHpeM38Scq3KbD4aqT6XXnLQa',
         RESPONSE_TYPE: 'id_token token',
-        REDIRECT_URL: 'https://verificacion_externa.portaloas.udistrital.edu.co/',
+        REDIRECT_URL: 'https://verificacionexterna.portaloas.udistrital.edu.co',
     },
 };
