f26db12ad3eec0133a331aa459e795ec954d94a4
JulianAvella <julianavella01@gmail.com>
2025-08-28 09:47:22 -0500

    feat: Se ajustan los archivos environment para solucionar error udistrital/sga_documentacion#626

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,7 +3,8 @@ export const environment = {
     apiUrl: "https://pruebasverificacionexterna.portaloas.udistrital.edu.co",
     AUTENTICACION_MID: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/',
     FIRMA_ELECTRONICA_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/firma_electronica_mid/v1/',
-    VERIFICACION_FIRMA_SERVICE: 'http://pruebasapi3.intranetoas.udistrital.edu.co:8584/v1/',
+    //VERIFICACION_FIRMA_SERVICE: 'http://pruebasapi3.intranetoas.udistrital.edu.co:8584/v1/',
+    VERIFICACION_FIRMA_SERVICE:'https://autenticacion.portaloas.udistrital.edu.co/apioas/verificacion_firma_digital_mid/v1/',
     SECRET_KEY_: 'NMJj8EKgM2iYjxTDJAuHXCAlopMa',
     TOKEN: {
         CLIENTE_ID: '8F0cB3XNG1bY9Quz_utqRDE9yh4a',
--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -3,7 +3,8 @@ export const environment = {
     apiUrl: "https://verificacionexterna.portaloas.udistrital.edu.co",
     AUTENTICACION_MID: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/',
     FIRMA_ELECTRONICA_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/firma_electronica_mid/v1/',
-    VERIFICACION_FIRMA_SERVICE: 'http://pruebasapi3.intranetoas.udistrital.edu.co:8584/v1/',
+    //VERIFICACION_FIRMA_SERVICE: 'http://pruebasapi3.intranetoas.udistrital.edu.co:8584/v1/',
+    VERIFICACION_FIRMA_SERVICE:'https://autenticacion.portaloas.udistrital.edu.co/apioas/verificacion_firma_digital_mid/v1/',
     SECRET_KEY_: 'JuMusTq5jWh8yIdTeqQxN7uYt6Ia',
     TOKEN: {
         CLIENTE_ID: 'q5NoHpeM38Scq3KbD4aqT6XXnLQa',
